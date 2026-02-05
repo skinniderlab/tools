612a5fe7c4d061a9715b7c89a4395887b8b35acd
anushka255 <aa9078@princeton.edu>
2026-02-05 11:02:25 -0500

    adding test file

---
+++ tests/test_peak.py
@@ -0,0 +1,48 @@
+from tools import Peak
+
+
+def test_peaks_object(isotope_db, peaks):
+    peak = Peak(
+        id=1,
+        mz=288.908623547669,
+        rt=64.8,
+        isotope_db=isotope_db,
+    )
+
+    assert peak in peaks, "Peaks object returned inaccurate membership result. "
+    assert 1 in peaks, "Peak object returned inaccurate membership result. "
+
+
+def test_peak_instantiation(isotope_db, peaks):
+    peak_1 = Peak(
+        id=28,
+        mz=256.961023547669,
+        rt=109.2,
+        level=4.0,
+        annotation="HMDB0060649 Ascorbic acid 2-sulfate C6H8O9S1 + O1 -> C6H8O10S1 - C1H2 -> C5H6O10S1",
+        formula_str="C5H6O10S1",
+        isotope_db=isotope_db,
+    )
+    peak_2 = Peak(
+        id=1089,
+        mz=387.096923547669,
+        rt=159.0,
+        annotation="Peak 2807 C9H25N4O2P1 + H2K1O4P1 -> C9H27K1N4O6P2",
+        formula_str="C9H27O6N4P2K",
+        isotope_db=isotope_db,
+    )
+
+    assert peaks[28] == peak_1
+    assert peaks[1089] == peak_2
+
+
+def test_peak_2(peaks_2, isotope_db):
+    peak = Peak(
+        id="131.046249_11.059",
+        mz=131.046249,
+        rt=11.059,
+        isotope_db=isotope_db,
+    )
+    assert peaks_2["131.046249_11.059"] == peak
+    assert peak in peaks_2
+    assert "131.046249_11.059" in peaks_2
